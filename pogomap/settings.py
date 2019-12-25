9b31dd9e5500a36f3817e880d0ed1a1376805788
manchos <vladm.ru@gmail.com>
2019-12-25 12:01:32 +0300

    delete magic exposed request in the models

--- pogomap/settings.py
+++ pogomap/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pokemon_entities.exposed_request.RequestExposerMiddleware'
 ]
 
 ROOT_URLCONF = 'pogomap.urls'
